f78a94036e6bd088c1b3c4c21cf44899fe2dd5d2
RasmusSpangsberg <rasmus.spangsberg@hotmail.com>
2019-09-18 22:38:32 +0200

    TODO ikke færdig

---
+++ opgave5.py
@@ -0,0 +1,11 @@
+# Klasser (på engelsk classes) er det sidste
+# vi skal lære om inden vi kan begynde at 
+# lave vores eget spil.
+
+# TODO TODO TODO TODO
+
+class Car:
+	def __init__(self, name, ):
+		print("Denne bil hedder", name)
+
+bil = Car("Tesla")
